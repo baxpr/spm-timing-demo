57c17e2f2c765553bfc955b222f8131c622631ba
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2024-04-19 09:30:42 -0500

    init

---
+++ timing_demo.m
@@ -0,0 +1,11 @@
+% Look at effects of stimulus timing on HRF model
+
+% Overview of design power/efficiency
+%    https://imaging.mrc-cbu.cam.ac.uk/imaging/DesignEfficiency
+
+% Metric for efficiency for a specific design matrix X and contrast C
+%   e(C,X) = trace( C'*inv(X'X)*C )^-1
+
+% How does efficiency change with scan time, e.g. by repeating a block 1..N
+% times?
+
